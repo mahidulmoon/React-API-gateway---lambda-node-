7139cd8d02061ac57c19594856b1cb759ce22272
mahidul <mahidulmoon@gmail.com>
2021-06-14 09:50:34 +0600

    raw lambda call with api gateway

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { API } from 'aws-amplify';
+import axios from 'axios' ;
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+const getData = () =>{
+  //axios.get('https://5tua63v9d7.execute-api.us-east-1.amazonaws.com/dev/testcors').then(res => console.log("response",res)).catch(err => console.log("error",err));
+  API.get('triblePostInsertAPI','/insertpost').then(response => console.log("lambda response",response)).catch(err => console.log("Error",err));
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello</h2>
+      <button onClick={getData}>Api Get</button>
+      {/* <AmplifySignOut /> */}
     </div>
   );
 }
 
+//export default withAuthenticator(App);
 export default App;
